df8932f06e3f46d25a7de4543fe64223ff92e23f
kurt miller <kmiller9393@gmail.com>
2019-09-28 17:24:17 -0600

    Create Product model and populate index.js with products

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   env: {
     MONGO_SRV:
-      'mongodb+srv://kmiller9393:p%40ssw0rd%279%27%21@loungearound-nmrye.mongodb.net/test?retryWrites=true&w=majority',
+      'mongodb+srv://kmiller9393:CUbuffs93%24@loungearound-nmrye.mongodb.net/test?retryWrites=true&w=majority',
     JWT_SECRET: '<insert-jwt-secret>',
     CLOUDINARY_URL: '<insert-cloudinary-url>',
     STRIPE_SECRET_KEY: '<insert-stripe-secret-key>'
