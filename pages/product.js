975a6d12417e37d0047475246cde5bfa68eecd96
kurt miller <kmiller9393@gmail.com>
2019-09-28 22:07:14 -0600

    Add logic to update baseUrl based on environment and refactor index.js and product.js with this logic

--- pages/product.js
+++ pages/product.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
+import ProductSummary from '../components/Product/ProductSummary';
+import ProductAttributes from '../components/Product/ProductAttributes';
+import baseUrl from '../utils/baseUrl';
 
 const Product = ({ product }) => {
-  return <>product</>;
+  return (
+    <>
+      <ProductSummary {...product} />
+      <ProductAttributes {...product} />
+    </>
+  );
 };
 
 Product.getInitialProps = async ({ query: { _id } }) => {
-  const url = 'http://localhost:3000/api/product';
+  const url = `${baseUrl}/api/product`;
   const payload = { params: { _id } };
   const response = await axios.get(url, payload);
   return { product: response.data };
