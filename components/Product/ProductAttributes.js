3ee6583aa507fc3f01c34abe412e3ace54615a8a
kurt miller <kmiller9393@gmail.com>
2019-09-28 22:05:48 -0600

    Add Header, description and Button to ProductAttributes component

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,5 +1,17 @@
-function ProductAttributes() {
-  return <>ProductAttributes</>;
-}
+import { Header, Button } from 'semantic-ui-react';
+
+const ProductAttributes = ({ description }) => {
+  return (
+    <>
+      <Header as="h3">About This Product</Header>
+      <p>{description}</p>
+      <Button
+        icon="trash alternate outline"
+        color="red"
+        content="Delete Product"
+      />
+    </>
+  );
+};
 
 export default ProductAttributes;
