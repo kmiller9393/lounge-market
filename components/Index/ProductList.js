94f995542370be4728ec541b73c28a8be60fe2c7
kurt miller <kmiller9393@gmail.com>
2019-09-28 18:24:16 -0600

    Create ProductList component and product Cards

--- components/Index/ProductList.js
+++ components/Index/ProductList.js
@@ -1,5 +1,18 @@
-function ProductList() {
-  return <>ProductList</>;
-}
+import { Card } from 'semantic-ui-react';
+
+const ProductList = ({ products }) => {
+  const mapProductsToItems = products => {
+    return products.map(product => ({
+      header: product.name,
+      image: product.mediaUrl,
+      color: 'teal',
+      fluid: true,
+      childKey: product._id,
+      href: `/product?_id=${product._id}`
+    }));
+  };
+
+  return <Card.Group items={mapProductsToItems(products)} />;
+};
 
 export default ProductList;
