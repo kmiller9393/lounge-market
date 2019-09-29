d62a219046a94f9e31c3bdaf4f31496c34a2dfe8
kurt miller <kmiller9393@gmail.com>
2019-09-28 22:05:15 -0600

    Add Input to AddProductToCart component

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,5 +1,15 @@
-function AddProductToCart() {
-  return <>AddProductToCart</>;
-}
+import { Input } from 'semantic-ui-react';
+
+const AddProductToCart = () => {
+  return (
+    <Input
+      type="number"
+      min="1"
+      placeholder="Quantity"
+      value={1}
+      action={{ color: 'orange', content: 'Add to Cart', icon: 'plus cart' }}
+    />
+  );
+};
 
 export default AddProductToCart;
