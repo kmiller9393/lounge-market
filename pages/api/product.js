d6c45ee8263e656a27f20d97f6d36e91832bb096
kurt miller <kmiller9393@gmail.com>
2019-10-16 21:38:35 -0600

    Add route handler (switch statement) to execute two methods based on the incoming request method

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,7 +1,28 @@
 import Product from '../../models/Product';
 
 export default async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      await handleGetRequest(req, res);
+      break;
+    case 'DELETE':
+      await handleDeleteRequest(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`);
+      break;
+    //if attempting to implement a method that is NOT a GET or DELETE method
+  }
+};
+
+const handleGetRequest = async (req, res) => {
   const { _id } = req.query;
   const product = await Product.findOne({ _id });
   res.status(200).json(product);
+};
+
+const handleDeleteRequest = async (req, res) => {
+  const { _id } = req.query;
+  await Product.findOneAndDelete({ _id });
+  res.status(204).json({});
 };
